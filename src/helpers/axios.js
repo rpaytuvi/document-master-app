54e474a6d87cb2892d4a72d51a970a370d7eb118
Rafael Paytuvi <rpaytuvi6486@gmail.com>
2021-09-26 18:20:17 +0200

    - Add axios - Axios helper first commit

---
+++ src/helpers/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const get = (url) => {
+  const options = {
+    url,
+    method: 'get',
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  };
+  return axios(options)
+    .then((response) => { return { ok: true, status: 200, data: response.data }; })
+    .catch((err) => {
+      const { response, message = 'Unexpected error' } = err || {};
+      const { status = 500, data } = response || {};
+      return { ok: false, status, data };
+    });
+}
+
+export default {
+  get,
+};
